13c88dff13bebdd7672f982d495f4f61805d766f
pfischof <pfischof@gmail.com>
2025-05-25 01:25:52 +0200

    fix: adjust margin in layout for improved spacing

--- app/page.tsx
+++ app/page.tsx
@@ -144,7 +144,7 @@ export default function Home() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="grid grid-cols-1 lg:grid-cols-3">
           {/* Left side - Credentials */}
-          <div className="lg:col-span-1 lg:mr-6">
+          <div className="lg:col-span-1 lg:mr-4">
             <Card className="sticky top-8">
               <div className="relative">
                 <Link
